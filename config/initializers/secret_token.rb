dfc6531a94299a569f76e43d370c94c8a9dd3ed9
Jatin Naik <jsnaik@gmail.com>
2014-01-19 11:17:55 +0530

    Init a rails app, without ActiveRecord and test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrunchbaseWebapp::Application.config.secret_key_base = 'becf4eca965974650edc0ee475c64349ed29a33d2f6c25a56a404958e2b32f6aa2e2beb52e1d30d2511d1349ffeaf9e9be45a474000837feb3d24887dc257b1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrunchbaseWebapp::Application.config.session_store :cookie_store, key: '_crunchbase_webapp_session'
